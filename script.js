0cd03b796e89f9bc44dae4c3ca699ee7af414e02
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2025-01-26 17:05:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
- const table = document.querySelector('table');
- const lastRow = document.createElement('tr');
- const prices = document.querySelectorAll('[data-ns-test=price]');
- let sum = 0;
- for(let i = 0; i < prices.length; i++) {
-  sum += parseInt(prices[i].textContent);
- }
- const child = document.createElement("td");
- child.setAttribute('data-ns-test', 'grandTotal');
- child.textContent = sum;
- lastRow.appendChild(child);
- table.appendChild(lastRow);
+const getSumBtn = document.createElement("button");
+getSumBtn.append("Get Total Price");
+document.body.appendChild(getSumBtn);
+const getSum = () => {
+//Add your code here
+const prices=document.getElementsByClassName('price');
+const pricesArr=Array.from(prices);
+let sum=0;
+pricesArr.forEach((elm)=>{
+    // console.log(typeof Number(elm.innerText)) 
+    sum+=Number(elm.innerText);
+})
+  const table=document.getElementsByTagName('table')[0];
+  const tr=document.createElement('tr');
+  const td=document.createElement('td');
+  td.innerText=`Total Price = ${sum}`;
+  td.colSpan='2';
+	td.id='ans';
+//   td.style.textAlign='justify';
+  tr.append(td);
+  table.append(tr);
+};
+getSumBtn.addEventListener("click", getSum);
